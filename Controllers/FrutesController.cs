90f249011d214d25dd6cf2b84af64c6f23562e1f
Sourav2032 <souravsenapati58207@gmail.com>
2024-06-23 22:05:46 +0530

    submit by sourav

---
+++ Controllers/FrutesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FrutesController : ControllerBase
+    {
+        public List<string> Frutes = new List<string>()
+        {
+            "apple",
+            "mango",
+            "banana",
+            "cherry",
+            "grapes"
+
+        };
+        [HttpGet]
+        public List<string> getFrutes()
+        {
+            return Frutes;
+        }
+
+        [HttpGet("{id}")]
+        public string getFrutesByIndex(int id)
+        {
+            return Frutes.ElementAt(id);
+        }
+
+
+    }
+}
